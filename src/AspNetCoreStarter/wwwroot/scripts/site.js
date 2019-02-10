54594dc1b7db8add80fb1ff57e4d3063daadb6ed
cjboyle <connorboyle@sasktel.net>
2019-02-09 21:21:49 -0600

    Added script to auto-insert red asterix to required form fields

--- src/AspNetCoreStarter/wwwroot/scripts/site.js
+++ src/AspNetCoreStarter/wwwroot/scripts/site.js
@@ -1,1 +1,19 @@
 ﻿/* Site-specific JavaScript */
+
+
+// Add red asterix to label of required fields
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('input').forEach(field => {
+        var req = field.hasAttribute('data-val-required');
+        if (req != undefined && req) {
+            var label = document.querySelectorAll('label[for="' + field.getAttribute('id') + '"]').item(0);
+            if (label != undefined) {
+                var text = label.innerHTML;
+                if (text.length > 0) {
+                    label.insertAdjacentHTML("beforeend", '<span class="has-text-danger"> *</span>');
+                }
+            }
+        }
+    });
+});
